4800e2e8c5720253080aa9b421dccae6b3c0ccb6
SanjayChetlure <sanjay.chetlure@gmail.com>
2026-01-01 21:13:05 +0530

    Selenium Repo created

---
+++ S1_Setup/Ex1_OpenBrowser.py
@@ -0,0 +1,13 @@
+import time
+
+from selenium import webdriver
+
+
+print("---Open Chrome browser-----")
+
+# driver=webdriver.Chrome()
+# driver=webdriver.Firefox()
+driver=webdriver.Edge()
+
+time.sleep(5)
+

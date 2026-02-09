c1e9cf2d5f1ab4fe015991ba43b541d4f70c6ff4
SanjayChetlure <sanjay.chetlure@gmail.com>
2026-02-09 21:15:29 +0530

    9th Feb 2026

---
+++ S2_HandlingOfDynamicElements/Ex2_getReviewFromFlipkartApp.py
@@ -0,0 +1,25 @@
+import time
+from selenium import webdriver
+from selenium.webdriver import ActionChains, Keys
+from selenium.webdriver.common.by import By
+
+driver=webdriver.Chrome()
+driver.maximize_window()
+driver.get("https://www.flipkart.com/")
+time.sleep(2)
+driver.implicitly_wait(10)
+
+#Enter mob name
+driver.find_element(By.XPATH,"//input[@class='lNPl8b']").send_keys("redmi 12 5G")
+
+#click on search btn
+driver.find_element(By.XPATH,"//button[@class='kV1UjG']").click()
+
+#get Ratings
+ratings=driver.find_element(By.XPATH,"((//div[@class='nZIRY7'])[1]//span[@class='PvbNMB']//span)[4]").text
+print(ratings)
+
+
+
+
+
